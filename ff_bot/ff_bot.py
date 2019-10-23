9e0cfa61b6e387b9dd4575f9e02de10fa2f14a61
rtrooler <31788111+rtrooler@users.noreply.github.com>
2019-10-23 15:08:12 -0700

    Update ff_bot.py

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -497,7 +497,7 @@ if __name__ == '__main__':
     #################################################################################################################################
     
     sched.add_job(bot_main, 'cron', ['get_randomsay'], id='randomsay',
-        day_of_week='wed', hour=17, minute=57, start_date=ff_start_date, end_date=ff_end_date,
+        day_of_week='wed', hour=18, minute=11, start_date=ff_start_date, end_date=ff_end_date,
         timezone=my_timezone, replace_existing=True)
         
     #sched.add_job(bot_main, 'cron', ['get_final'], id='final',
