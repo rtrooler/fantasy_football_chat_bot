1738291938c1f9c01507b7930134212d7c3a28c0
rtrooler <31788111+rtrooler@users.noreply.github.com>
2019-10-22 21:36:47 -0700

    Update ff_bot.py

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -433,7 +433,7 @@ if __name__ == '__main__':
     #s
     #s
     sched.add_job(bot_main, 'cron', ['random_phrase()'], id='phrase',
-        day_of_week='wed', hour=00, minute=34, start_date=ff_start_date, end_date=ff_end_date,
+        day_of_week='tue', hour=23, minute=38, start_date=ff_start_date, end_date=ff_end_date,
         timezone=my_timezone, replace_existing=True)
     
     print("Ready!")
