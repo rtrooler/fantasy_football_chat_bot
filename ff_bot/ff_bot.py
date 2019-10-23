3162f76185fab70ac93077fb9ff844b43712f3af
rtrooler <31788111+rtrooler@users.noreply.github.com>
2019-10-23 14:21:50 -0700

    Update ff_bot.py

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -497,7 +497,7 @@ if __name__ == '__main__':
         day_of_week='sun,mon', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date,
         timezone=game_timezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_final'], id='final',
-        day_of_week='wed', hour=17, minute=20, start_date=ff_start_date, end_date=ff_end_date,
+        day_of_week='wed', hour=16, minute=25, start_date=ff_start_date, end_date=ff_end_date,
         timezone=my_timezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard1',
         day_of_week='fri,mon', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date,
