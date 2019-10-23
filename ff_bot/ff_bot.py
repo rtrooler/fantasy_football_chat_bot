81068717596dfc678845147f260b692beae91618
rtrooler <31788111+rtrooler@users.noreply.github.com>
2019-10-23 16:01:48 -0700

    Update ff_bot.py

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -225,14 +225,14 @@ def get_scoreboard_short(league, week=None):
 #TEST RANDOMSAY
 
 
-def get_randomsay(league, week=None):
+#def get_randomsay(league, week=None):
     #Gets current week's scoreboard
-    box_scores = league.box_scores(week=week)
-    score = ['%s %.2f - %.2f %s' % (i.home_team.team_abbrev, i.home_score,
+   # box_scores = league.box_scores(week=week)
+   # score = ['%s %.2f - %.2f %s' % (i.home_team.team_abbrev, i.home_score,
              i.away_score, i.away_team.team_abbrev) for i in box_scores
              if i.away_team]
-    text = ['From the archives:'] + random_phrase()
-    return '\n'.join(text)
+   # text = ['From the archives:'] + random_phrase()
+  #  return '\n'.join(text)
 
 
 ################################################################################################
@@ -429,8 +429,7 @@ def bot_main(function):
         text = get_randomsay(league)
         text = text
     elif function=="get_randomsay":
-        text = get_randomsay(league)
-        text = text
+        text = ['From the archives:'] + random_phrase()
     elif function=="get_projected_scoreboard":
         text = get_projected_scoreboard(league)
     elif function=="get_close_scores":
@@ -514,12 +513,9 @@ if __name__ == '__main__':
     
     #################################################################################################################################
     
-    sched.add_job(bot_main, 'cron', ['get_randomsay'], id='randomsay',
-        day_of_week='mon,tue,wed,fri,sat', hour='11,15,19', minute=35, start_date=ff_start_date, end_date=ff_end_date,
-        timezone=my_timezone, replace_existing=True)
     
     sched.add_job(bot_main, 'cron', ['get_randomsay'], id='randomsay',
-        day_of_week='wed', hour='17,18,19', minute=26, start_date=ff_start_date, end_date=ff_end_date,
+        day_of_week='wed', hour='17,18,19,20', minute=3, start_date=ff_start_date, end_date=ff_end_date,
         timezone=my_timezone, replace_existing=True)
         
     #sched.add_job(bot_main, 'cron', ['get_final'], id='final',
